7790a6c22c30bfc352a1a1a31939d33e60f9d13b
Lukacs Papp <lukacs.papp@jentis.com>
2024-04-15 11:46:43 +0200

    done

--- src/schema.ts
+++ src/schema.ts
@@ -1,46 +1,202 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import typeDefs from './schema.graphql';
-
-type Link = {
-  id: string;
-  url: string;
-  description: string;
-}
-
-const links: Link[] = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+import { GraphQLContext } from "./context";
+import typeDefs from "./schema.graphql";
+import { Link, Prisma, User } from "@prisma/client";
+import { compare, hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { APP_SECRET } from "./auth";
+import { PubSubChannels } from "./pubsub";
 
 const resolvers = {
+  User: {
+    links: (parent: User, args: {}, context: GraphQLContext) =>
+      context.prisma.user.findUnique({ where: { id: parent.id } }).links(),
+  },
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: async (
+      parent: unknown,
+      args: {
+        filter?: string;
+        skip?: number;
+        take?: number;
+        orderBy?: {
+          description?: Prisma.SortOrder;
+          url?: Prisma.SortOrder;
+          createdAt?: Prisma.SortOrder;
+        };
+      },
+      context: GraphQLContext
+    ) => {
+      const where = args.filter
+        ? {
+          OR: [
+            { description: { contains: args.filter } },
+            { url: { contains: args.filter } },
+          ],
+        }
+        : {};
+
+      const totalCount = await context.prisma.link.count({ where });
+      const links = await context.prisma.link.findMany({
+        where,
+        skip: args.skip,
+        take: args.take,
+        orderBy: args.orderBy,
+      });
+
+      return {
+        count: totalCount,
+        links,
+      };
+    },
+    me: (parent: unknown, args: {}, context: GraphQLContext) => {
+      if (!context.currentUser) {
+        throw new Error("Not authenticated");
+      }
+
+      return context.currentUser;
+    }
   },
   Link: {
     id: (parent: Link) => parent.id,
     description: (parent: Link) => parent.description,
     url: (parent: Link) => parent.url,
+    postedBy: async (parent: Link, args: {}, context: GraphQLContext) => {
+      if (!context.currentUser) {
+        throw new Error("Not authenticated");
+      }
+      return context.prisma.link
+        .findUnique({ where: { id: parent.id } })
+        .postedBy();
+    },
+    votes: (parent: Link, args: {}, context: GraphQLContext) =>
+      context.prisma.link.findUnique({ where: { id: parent.id } }).votes(),
   },
   Mutation: {
-    post: (parent: unknown, args: { description: string, url: string }) => {
-      // 1
-      let idCount = links.length;
+    signup: async (
+      parent: unknown,
+      args: { email: string; password: string },
+      context: GraphQLContext
+    ) => {
 
-      // 2
-      const link: Link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
+      const password = await hash(args.password, 10)
+
+      const user = await context.prisma.user.create({
+        //@ts-expect-error
+        data: { ...args, password },
+      });
+
+      const token = sign({ userId: user.id }, APP_SECRET);
+
+      return {
+        token,
+        user,
+      }
+    },
+    login: async (
+      parent: unknown,
+      args: { email: string; password: string },
+      context: GraphQLContext
+    ) => {
+      const user = await context.prisma.user.findUnique({
+        where: { email: args.email },
+      });
+
+      if (!user) {
+        throw new Error("No such user found");
+      }
+
+      const valid = await compare(args.password, user.password);
+
+      if (!valid) {
+        throw new Error("Invalid password");
+      }
+
+      const token = sign({ userId: user.id }, APP_SECRET);
+
+      return {
+        token,
+        user,
       };
+    },
+    post: (
+      parent: unknown,
+      args: { description: string; url: string },
+      context: GraphQLContext
+    ) => {
 
-      links.push(link);
+      if (!context.currentUser) {
+        throw new Error("Not authenticated");
+      }
 
-      return link;
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+          postedBy: { connect: { id: context.currentUser?.id } },
+        },
+      });
+
+      //@ts-expect-error
+      context.pubSub.publish("newLink", { createdLink: newLink });
+
+      return newLink;
+    },
+    vote: async (
+      parent: unknown,
+      args: { linkId: number },
+      context: GraphQLContext
+    ) => {
+      if (!context.currentUser) {
+        throw new Error("Not authenticated");
+      }
+
+      const userId = context.currentUser.id;
+
+      const vote = await context.prisma.vote.findUnique({
+        where: {
+          linkId_userId: {
+            linkId: Number(args.linkId),
+            userId,
+          },
+        },
+      });
+
+      if (vote !== null) {
+        throw new Error(`Already voted for link: ${args.linkId}`);
+      }
+
+      const newVote = context.prisma.vote.create({
+        data: {
+          user: { connect: { id: userId } },
+          link: { connect: { id: Number(args.linkId) } },
+        },
+      });
+      //@ts-expect-error
+      context.pubSub.publish("newVote", { createdVote: newVote });
+
+      return newVote;
+
     },
   },
-}
+  Subscription: {
+    newLink: {
+      subscribe: (parent: unknown, args: {}, context: GraphQLContext) => {
+        return context.pubSub.asyncIterator("newLink");
+      },
+      resolve: (payload: PubSubChannels["newLink"][0]) => {
+        return payload.createdLink;
+      },
+    },
+  },
+  Vote: {
+    link: (parent: User, args: {}, context: GraphQLContext) =>
+      context.prisma.vote.findUnique({ where: { id: parent.id } }).link(),
+    user: (parent: User, args: {}, context: GraphQLContext) =>
+      context.prisma.vote.findUnique({ where: { id: parent.id } }).user(),
+  },
+};
 
 export const schema = makeExecutableSchema({
   typeDefs,
--- src/script.ts
+++ src/script.ts
@@ -16,6 +16,7 @@ async function main() {
   console.log(allLinks)
 }
 
+
 // 4
 main()
   .catch(e => {
